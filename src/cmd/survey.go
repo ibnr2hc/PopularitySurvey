ff0e45687c962aa212d29b75f25182372cb954df
Takumi Suzuki <43519598+ibnr2hc@users.noreply.github.com>
2022-11-30 23:26:18 +0900

    [#1] surveyコマンドのMockを作成した

---
+++ src/cmd/survey.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var surveyCmd = &cobra.Command{
+	Use:   "survey",
+	Short: "指定したユーザーのフォロワーを調査しランキングにする。",
+	Long: `指定したユーザーのフォロワーを調査しランキングにする。
+フォロワー数が多い順にソートして表示をする。`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		//screenName, err := cmd.Flags().GetString("screen_name")
+		//if err != nil {
+		//	fmt.Println(err)
+		//}
+		fmt.Println("調査対象：ほにゃらら(@xxx) フォロワー数100人")
+		fmt.Println("人気フォロワー1位：あれこれ(@xxx) フォロワー数1000人")
+		fmt.Println("人気フォロワー2位：だれそれ(@xxx) フォロワー数900人")
+		fmt.Println("人気フォロワー3位：それそれ(@xxx) フォロワー数800人")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(surveyCmd)
+
+	// surveyCmd.Flags().StringP("screen_name", "s", "", "Twitter user screen name")
+}
