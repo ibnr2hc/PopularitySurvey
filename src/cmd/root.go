a5dde7e9d2bee65b16fa339743d1575eef418dfc
Takumi Suzuki <43519598+ibnr2hc@users.noreply.github.com>
2023-01-18 05:42:30 +0900

    [#21] ツールの説明文を修正した

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -8,16 +8,9 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "PopularitySurvey",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "指定したユーザーのフォロワーをランキング付けする",
+	Long: `指定したユーザーのフォロワーをランキング付けする。
+フォロワー数をソートし、上位から数十名を表示する。`,
 }
 
 func Execute() {
